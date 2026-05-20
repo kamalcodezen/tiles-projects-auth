9390f1cc1027cd72e70f7e30f0940012c5dde82a
kamalcodezen <kamaluddin7908@gmail.com>
2026-05-20 23:33:25 +0530

    form create

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,11 +1,85 @@
-
+import { Eye, EyeSlash } from "@gravity-ui/icons";
+import {
+  Button,
+  FieldError,
+  Form,
+  Input,
+  InputGroup,
+  Label,
+  TextField,
+} from "@heroui/react";
+import { useState } from "react";
 
 const SIgnInPage = () => {
-    return (
-        <div>
-            <h2>This is Sign in page</h2>
-        </div>
-    );
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div>
+      <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+        {/* name */}
+        <TextField
+          isRequired
+          name="name"
+          validate={(value) => {
+            if (value.length < 3) {
+              return "Name must be at least 3 characters";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Name</Label>
+          <Input name="name" placeholder="John Doe" />
+          <FieldError />
+        </TextField>
+
+        {/* Email */}
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input name="email" placeholder="john@example.com" />
+          <FieldError />
+        </TextField>
+
+        {/* password */}
+        <TextField className="w-full max-w-[280px]" name="password">
+          <Label>Password</Label>
+          <InputGroup>
+            <InputGroup.Input
+              className="w-full max-w-[280px]"
+              type={isVisible ? "text" : "password"}
+              name="password"
+            />
+            <InputGroup.Suffix className="pr-0">
+              <Button
+                isIconOnly
+                aria-label={isVisible ? "Hide password" : "Show password"}
+                size="sm"
+                variant="ghost"
+                onPress={() => setIsVisible(!isVisible)}
+              >
+                {isVisible ? (
+                  <Eye className="size-4" />
+                ) : (
+                  <EyeSlash className="size-4" />
+                )}
+              </Button>
+            </InputGroup.Suffix>
+          </InputGroup>
+        </TextField>
+      </Form>
+    </div>
+  );
 };
 
 export default SIgnInPage;
