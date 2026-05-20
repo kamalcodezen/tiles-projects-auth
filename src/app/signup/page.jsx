2c7d67d40f3f8efebea6d75c9860da72a01a60ff
kamalcodezen <kamaluddin7908@gmail.com>
2026-05-21 01:11:41 +0530

    signUp succesful

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,11 +1,139 @@
+"use client";
 
+import { Check, Eye, EyeSlash } from "@gravity-ui/icons";
+import {
+  Button,
+  FieldError,
+  Form,
+  Input,
+  InputGroup,
+  Label,
+  TextField,
+} from "@heroui/react";
+import { useState } from "react";
+
+import { useRouter } from "next/navigation";
+import { authClient } from "@/lib/auth-client";
 
 const SignUpPage = () => {
-    return (
-        <div>
-            <h2>This is Sign Up page</h2>
-        </div>
+  const [isVisible, setIsVisible] = useState(false);
+  const router = useRouter();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const userData = Object.fromEntries(formData.entries());
+
+    const { data, error } = await authClient.signUp.email(
+      {
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+        callbackURL: "/",
+      },
+      {
+        onRequest: () => {
+          console.log("Loading...");
+        },
+        onSuccess: () => {
+          console.log("Signup successful");
+          router.push("/");
+        },
+        onError: (ctx) => {
+          console.log(ctx.error.message);
+        },
+      },
     );
+
+    // if (error) {
+    //   console.log(error.message);
+    // } else {
+    //   console.log(data);
+
+    //   router.push("/");
+    // }
+  };
+
+  return (
+    <div>
+      <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+        {/* name */}
+        <TextField
+          isRequired
+          name="name"
+          validate={(value) => {
+            if (value.length < 3) {
+              return "Name must be at least 3 characters";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Name</Label>
+          <Input name="name" placeholder="John Doe" />
+          <FieldError />
+        </TextField>
+
+        {/* Email */}
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input name="email" placeholder="john@example.com" />
+          <FieldError />
+        </TextField>
+
+        {/* password */}
+        <TextField className="w-full max-w-[280px]" name="password">
+          <Label>Password</Label>
+          <InputGroup>
+            <InputGroup.Input
+              className="w-full max-w-[280px]"
+              type={isVisible ? "text" : "password"}
+              name="password"
+              placeholder="Your Password"
+            />
+            <InputGroup.Suffix className="pr-0">
+              <Button
+                isIconOnly
+                aria-label={isVisible ? "Hide password" : "Show password"}
+                size="sm"
+                variant="ghost"
+                onPress={() => setIsVisible(!isVisible)}
+              >
+                {isVisible ? (
+                  <Eye className="size-4" />
+                ) : (
+                  <EyeSlash className="size-4" />
+                )}
+              </Button>
+            </InputGroup.Suffix>
+          </InputGroup>
+        </TextField>
+
+        <div className="flex gap-2">
+          <Button type="submit">
+            <Check />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
 };
 
 export default SignUpPage;
