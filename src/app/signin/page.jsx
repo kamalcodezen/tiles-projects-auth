fff0108c83167e02367f8acaf01a5580df3efb5c
kamalcodezen <kamaluddin7908@gmail.com>
2026-05-21 02:36:01 +0530

    signIn complete

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -1,10 +1,117 @@
+"use client";
 
-const page = () => {
-    return (
-        <div>
-            <h2>Sign in</h2>
-        </div>
+import { authClient } from "@/lib/auth-client";
+import { Check, Eye, EyeSlash } from "@gravity-ui/icons";
+import {
+  Button,
+  FieldError,
+  Form,
+  Input,
+  InputGroup,
+  Label,
+  TextField,
+} from "@heroui/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+const SignInPage = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const router = useRouter();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    // console.log(formData);
+    const userData = Object.fromEntries(formData.entries());
+    // console.log(userData, "userData");
+
+    const { data, error } = await authClient.signIn.email(
+      {
+        // name: userData.name,
+        email: userData.email,
+        password: userData.password,
+        callbackURL: "/",
+      },
+      {
+        onRequest: () => {
+          //show loading
+          console.log("Loading....");
+        },
+        onSuccess: () => {
+          //redirect to the dashboard or sign in page
+          console.log("Login Successful");
+
+          router.push("/");
+        },
+        onError: (ctx) => {
+          // display the error message
+          console.log(ctx.error.message);
+        },
+      },
     );
+  };
+
+  return (
+    <div>
+      <h2>Sign in Page</h2>
+      <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit}>
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input name="email" placeholder="john@example.com" />
+          <FieldError />
+        </TextField>
+
+        <TextField className="w-full max-w-[280px]" name="password">
+          <Label>Password</Label>
+          <InputGroup>
+            <InputGroup.Input
+              className="w-full max-w-[280px]"
+              type={isVisible ? "text" : "password"}
+              name="password"
+              placeholder="Password"
+            />
+
+            <InputGroup.Suffix className="pr-0">
+              <Button
+                isIconOnly
+                aria-label={isVisible ? "Hide password" : "Show password"}
+                size="sm"
+                variant="ghost"
+                onPress={() => setIsVisible(!isVisible)}
+              >
+                {isVisible ? (
+                  <Eye className="size-4" />
+                ) : (
+                  <EyeSlash className="size-4" />
+                )}
+              </Button>
+            </InputGroup.Suffix>
+          </InputGroup>
+        </TextField>
+
+        <div className="flex gap-2">
+          <Button type="submit">
+            <Check />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
 };
 
-export default page;
+export default SignInPage;
